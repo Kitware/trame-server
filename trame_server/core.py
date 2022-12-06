005192d508d9abe1a3d8557637c3e7163d1048f1
jwindgassen <75307755+jwindgassen@users.noreply.github.com>
2022-12-06 16:10:12 +0100

    chore(security): Fixed Formatting

--- trame_server/core.py
+++ trame_server/core.py
@@ -278,7 +278,9 @@ class Server:
         )
         self._cli_parser.add_argument(
             "--authKeyFile",
-            help="Path to a File that contains the Authentication key for clients to connect to the WebSocket. This takes precedence over '-a, --authKey' from wslink."
+            help="""Path to a File that contains the Authentication key for clients
+                    to connect to the WebSocket. 
+                    This takes precedence over '-a, --authKey' from wslink.""",
         )
 
         CoreServer.add_arguments(self._cli_parser)
