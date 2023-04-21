05e2b2854386b04463788145af400a341628ee68
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-04-21 17:44:23 -0600

    feat(py-client): add Python client to drive remote state

--- trame_server/__init__.py
+++ trame_server/__init__.py
@@ -1,8 +1,10 @@
 from .core import Server
+from .client import Client
 from .utils.version import get_version
 
 __version__ = get_version("trame-server")
 __license__ = "Apache License 2.0"
 __all__ = [
     "Server",
+    "Client",
 ]
