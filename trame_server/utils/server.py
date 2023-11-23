5d03ffa73e87a9138e2e1dd0b6be77499143a238
Bane Sullivan <banesullivan@gmail.com>
2023-11-23 00:25:47 -0800

    Improve print_informations

--- trame_server/utils/server.py
+++ trame_server/utils/server.py
@@ -3,18 +3,21 @@ import socket
 
 
 def print_informations(server):
-    args = server.cli.parse_known_args()[0]
-    local_url = f"http://{args.host}:{args.port}/"
+    options = server.server_options
+    local_url = f"http://{options.host}:{server.port}/"
     print()
     print("App running at:")
     print(f" - Local:   {local_url}")
 
     try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
+        try:
+            host_ip = socket.gethostbyname(options.host)
+        except Exception:
+            host_name = socket.gethostname()
+            host_ip = socket.gethostbyname(host_name)
         print(f" - Network: http://{host_ip}:{server.port}/")
     except socket.gaierror:
-        print(f" - Network: http://{args.host}:{server.port}/")
+        print(f" - Network: http://{options.host}:{server.port}/")
         pass
 
     print()
