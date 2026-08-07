1bbd7525fe4db45a0f8ef3bf688b4d9e86c3d2ee
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-08-07 16:13:21 -0400

    fix(state.change): ignore exception in callback

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
 from trame_server import Server
+from trame_server.core import Translator
+from trame_server.state import State
 
 
 @pytest.fixture(scope="session")
@@ -11,3 +13,37 @@ def server():
 @pytest.fixture
 def controller(server):
     return server.controller
+
+
+class FakeServer:
+    def __init__(self):
+        self._change_callbacks = {}
+        self._events = []
+        self.translator = Translator()
+        self.state = State(commit_fn=self._push_state, hot_reload=False)
+
+    def _push_state(self, delta_state):
+        self._events.append({"type": "push", "content": {**delta_state}})
+
+    def add_event(self, content, type="msg"):
+        self._events.append({"type": type, "content": content})
+
+    @property
+    def pushed_state(self) -> dict:
+        pushed = {}
+        for e in self._events:
+            if e["type"] == "push":
+                pushed.update(e["content"])
+        return pushed
+
+    def __repr__(self) -> str:
+        lines = [""]
+        for line_nb, entry in enumerate(self._events):
+            lines.append(f"{line_nb:6} {entry.get('type'):5}: {entry.get('content')}")
+        lines.append("")
+        return "\n".join(lines)
+
+
+@pytest.fixture
+def fake_server():
+    return FakeServer()
